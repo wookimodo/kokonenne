b4cd54d3d1b183ac4d41dfaa9f215024d950b231
channy <jya05064@naver.com>
2023-01-11 20:55:17 +0900

    feat: 데이터 불러오기 성공 stacks 빼고

--- ofcourse/course/views.py
+++ ofcourse/course/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Course, Stacks
+# from django.views.generic import ListView
+
+def list(requests):
+    # db에서 data query해서 오기
+    course = Course.objects.all()
+    stacks = Stacks.objects.all()
+    return render(requests, 'course/list.html', {"course":course, "stacks":stacks})
 
 # Create your views here.
