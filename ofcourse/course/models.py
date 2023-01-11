55dd7ee6f86ffc407a9b56daa5ff17df6ebdfe81
LAPTOP-87NET8S1\playdata <jya05064@naver.com>
2023-01-11 14:05:48 +0900

    feat: 모델수정 및 db저장 코드 생성

--- ofcourse/course/models.py
+++ ofcourse/course/models.py
@@ -7,19 +7,19 @@ import uuid
 
 # 강의 모델
 class Course(models.Model):
-    urlid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    urlid = models.CharField(primary_key=True, default=None,max_length=100)
     url = models.URLField(max_length=100,default=None)
     img_url = models.URLField(max_length=250,default=None)
     title = models.CharField(max_length=30,default=None)
     teacher = models.CharField(max_length=30,default='')
     headline = models.TextField(default='')
     level = models.CharField(max_length=10,default='')
-    score = models.IntegerField(default=0)
+    score = models.FloatField(default=0, null=True)
     courseTime = models.CharField(max_length=50,default='')
-    stduentCnt = models.IntegerField(default=0)
-    recommend = models.IntegerField(default=0)
-    reviewCnt = models.IntegerField(default=0)
-    price = models.IntegerField(default='')
+    studentCnt = models.IntegerField(default=0,null=True)
+    recommend = models.IntegerField(default=0,null=True)
+    reviewCnt = models.IntegerField(default=0,null=True)
+    price = models.IntegerField(default='',null=True)
 
 # 기술스택 모델
 
---
+++ ofcourse/db_save.py
@@ -0,0 +1,41 @@
+import json
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ofcourse.settings")
+import django
+django.setup()
+from course.models import Course, Stacks
+
+f = open(f'modify.json', encoding='UTF-8')
+data = json.loads(f.read())
+
+# Course 테이블 저장
+for i in data:
+    urlid = data[i]['pwdurl']
+    url = data[i]['url']
+    img_url = data[i]['image']
+    title = i
+    teacher = data[i]['teacher']
+    headline = data[i]['headline']
+    level = data[i]['level']
+    score = data[i]['score']
+    courseTime = data[i]['courseTime']
+    studentCnt = data[i]['student']
+    recommend = data[i]['recommend']
+    reviewCnt = data[i]['reviewCnt']
+    price = data[i]['price']
+
+    Course(urlid=urlid, 
+            url=url, 
+            img_url=img_url, 
+            title=title,
+            teacher=teacher,
+            headline=headline,
+            level=level,
+            score=score,
+            courseTime=courseTime,
+            studentCnt=studentCnt,
+            recommend=recommend,
+            reviewCnt=reviewCnt,
+            price=price
+            ).save()
+print("DB저장완료")
