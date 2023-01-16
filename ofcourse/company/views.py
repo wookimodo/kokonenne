914615e522739eaacfe533a905a4695ec4b963c8
Eunhyang <peh6405@gmail.com>
2023-01-16 19:20:15 +0900

    feat : company models 수정

--- ofcourse/company/views.py
+++ ofcourse/company/views.py
@@ -31,12 +31,3 @@ class Companydetail(DetailView):
         return context
 
 
-
-def business_list(request):
-   business_list = Business.objects.all().order_by('-id') #데이터 역순 정렬
-   page = request.GET.get('page', '1') #GET 방식으로 정보를 받아오는 데이터
-   paginator = Paginator(business_list, '2') #Paginator(분할될 객체, 페이지 당 담길 객체수)
-   paginated_business_lists = paginator.get_page(page) #페이지 번호를 받아 해당 페이지를 리턴
-   ctx = {'business_list':business_list,'paginated_business_lists':paginated_business_lists}
-
-   return render(request, template_name='list.html', context=ctx)
