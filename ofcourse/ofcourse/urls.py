0bff03263705e8ba219de9e3b725ff3936078970
Yeonguk Kim <rudghk1437@naver.com>
2023-01-18 19:41:21 +0900

    feat : 홈페이지 구현 및 url 설정

--- ofcourse/ofcourse/urls.py
+++ ofcourse/ofcourse/urls.py
@@ -16,10 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from course import views
+from course.views import index
 
 
 urlpatterns = [
+    path('', index, name='index'),
+
     path('admin/', admin.site.urls),
+
     # path('list/', list),
     path('course/', include('course.urls')),
 
@@ -27,5 +31,8 @@ urlpatterns = [
     path('company/', include('company.urls')),
 
     # stack
-    path('stack/', include('stack.urls'))
+    path('stack/', include('stack.urls')),
+
+    # search 
+    path('search/', views.search, name='search'),
 ]
