2c0b4ec38dc64331c42073c89f65a6ab5566e2c3
Yeonguk Kim <rudghk1437@naver.com>
2023-01-22 00:22:01 +0900

    feat : udemy data 적재

--- ofcourse/scripts/course_ml.py
+++ ofcourse/scripts/course_ml.py
@@ -11,39 +11,39 @@ def toJson(res_dict,save_name):
     with open(f'{save_name}.json', 'w', encoding='utf-8') as file :
         json.dump(res_dict, file, ensure_ascii=False, indent='\t')
 
-# 제이슨 파일 열기
-f = open(f'json/inflearn.json', encoding='UTF-8')
+# 전체 데이터
+f = open(f'json/allcourse.json', encoding='UTF-8')
 data = json.loads(f.read())  
 
-# 데이터 불러오기
-df = pd.read_json(f"json/inflearn.json")
-df = df.transpose().reset_index().rename(columns={'index':'title'})
-df = df.drop(['headline','url','image','level'],axis=1)
+# 데이터 불러오기(inflearn)
+df1 = pd.read_json("json/inflearn.json")
+df1 = df1.transpose().reset_index().rename(columns={'index':'title'})
+df1 = df1.drop(['headline','url','image','level','recommend','price','rank','courseTime'],axis=1)
+df1 = df1[['title','teacher','stacks','student','reviewCnt','score']]
+df1['website'] = 'inflearn'
 
-# 강의 수 컬럼 추가
-df['courseCnt'] = [i.lstrip('총').split('개')[0] for i in df['courseTime']]
+# 데이터 불러오기(udemy)
+df2 = pd.read_json("json/udemy.json")
+df2 = df2.transpose().reset_index().rename(columns={'index':'title'})
+df2 = df2.drop(['headline','url','image','level','courseTime','courseCnt','price','recommend'],axis=1)
+df2 = df2.rename(columns={'rating':'score'})
+df2 = df2[['title','teacher','stacks','student','reviewCnt','score']]
+df2['website'] = 'udemy'
 
-# 강의 시간 전처리
+# 데이터 합치기
+df = pd.concat([df1,df2],axis=0)
 
-df['courseTime'] = [i.split('개')[1].rstrip('의수업') for i in df['courseTime']]
-df['courseTime'] = [i.replace('시간','.').replace('분','') for i in df['courseTime']]
-
-# 강의 가격 전처리
-df['price'] = [i.replace(',','') if i != None else i for i in df['price']]
+# 리뷰 수 전처리 
+df['reviewCnt'] = [str(i).replace(',','') for i in df['reviewCnt']]
 
 # 정보없음, 무료, 수강생없음 등은 0으로 처리, 평점은 null값으로 대체
-df['student'] = [0 if i==None else i for i in df['student']]
-df['recommend'] = [0 if i==None else i for i in df['recommend']]
-df['reviewCnt'] = [0 if i==None else i for i in df['reviewCnt']]
-df['score'] = [None if i=='' else i for i in df['score']]
-df['price'] = [0 if i == None else i for i in df['price']]
-df['courseTime'] = [0 if i.isdigit()==False else i for i in df['courseTime']]
+df['student'] = [0 if i=='None' else i for i in df['student']]
+df['reviewCnt'] = [0 if i=='None' else i for i in df['reviewCnt']]
+df['score'] = [None if i=='None' else i for i in df['score']]
+
 
 # 타입변환
-df = df.astype({'courseTime':'float','student':'int','recommend':'int','reviewCnt':'int','price':'int','score':'float','courseCnt':'int'})
-
-# 강의당 시간 컬럼 추가
-df['pertime'] = [x/y for x,y in zip(df['courseTime'],df['courseCnt'])]
+df = df.astype({'student':'float','reviewCnt':'float','score':'float'})
 
 # stack 없는 행 삭제
 idx = df.loc[df['stacks']==""].index
@@ -55,21 +55,12 @@ for i in df['stacks']:
   cat1.append(i[0])
 df['cat1'] = cat1
 # 컬럼 순서 재정의
-df = df[['title','teacher','cat1','courseTime','student','recommend','reviewCnt','score','courseCnt','pertime']]
+df = df[['title','teacher','website','cat1','student','reviewCnt','score']]
 
 # 대분류별로 평균을 구해서 컬럼 추가
 agg_dict = {
-    'pertime' : [
-        ("avg_pertime", 'mean'),
-    ],
         'reviewCnt' : [
         ("avg_reviewCnt", 'mean'),
-    ],
-          'courseTime' : [
-        ("avg_courseTime", 'mean'),
-    ],
-          'recommend' : [
-        ("avg_recommend", 'mean'),
     ],
           'score' : [
         ("avg_score", 'mean'),
@@ -86,33 +77,35 @@ tmp = tmp.reset_index()
 
 # df에 merge
 df = df.merge(tmp,how="left",on = "cat1") 
-
 # 평점에 대한 결측치는 평균으로 채움
 df["score"] = df["score"].fillna(df["score"].mean())
 df["avg_score"] = df["avg_score"].fillna(df["avg_score"].mean())
 
-# 강의시간, 수강생수, 추천수, 리뷰수, 평점이 각 카테고리의 평균와 얼마나 차이나는지
+# student 결측치는 0으로 채움
+df["student"] = df["student"].fillna(0)
+df["avg_student"] = df["avg_student"].fillna(0)
+
+# 강의시간, 수강생수, 리뷰수, 평점이 각 카테고리의 평균와 얼마나 차이나는지
 df['diffstudent'] = [x-y for x,y in zip(df['student'],df['avg_student'])]
-df['diffrecommend'] = [x-y for x,y in zip(df['recommend'],df['avg_recommend'])]
 df['diffreviewCnt'] = [x-y for x,y in zip(df['reviewCnt'],df['avg_reviewCnt'])]
 df['diffscore'] = [x-y for x,y in zip(df['score'],df['avg_score'])]
 
 # 랭킹계산에 필요한 컬럼만 추출
-df_ranking = df.iloc[:,4:]
+df_ranking = df.iloc[:,5:]
 # 민맥스스케일링
 scaler = MinMaxScaler()
 df_ranking = scaler.fit_transform(df_ranking)
 
 df_ranking = pd.DataFrame(df_ranking)
-df_ranking.columns = df.iloc[:,4:].columns
+df_ranking.columns = df.iloc[:,5:].columns
 
 # 랭킹df에 title, teacher, cat1추가
-df_ranking[['title','teacher']] = df[['title','teacher']]
+df_ranking[['title','teacher','website']] = df[['title','teacher','website']]
 df_ranking['cat1'] = df['cat1']
-df = df_ranking[['title','teacher','cat1','diffstudent','diffrecommend','diffreviewCnt','diffscore']]
+df = df_ranking[['title','teacher','website','cat1','diffstudent','diffreviewCnt','diffscore']]
 
 # finalscore 계산
-df['finalscore'] = [a+b+c+d for a,b,c,d in zip(df.loc[:,'diffstudent'],df.loc[:,'diffrecommend'],df.loc[:,'diffreviewCnt'],df.loc[:,'diffscore'])]
+df['finalscore'] = [a+b+c if website == 'inflearn' else 0.95*(a+b+c) for a,b,c,website in zip(df.loc[:,'diffstudent'],df.loc[:,'diffreviewCnt'],df.loc[:,'diffscore'],df.loc[:,'website'])]
 df['rank'] = df['finalscore'].rank(ascending=False)
 
 # 기술 스택별로 랭킹반환하는 함수
@@ -122,7 +115,7 @@ def run():
     for i in data:
       if i == title:
         data[i]['rank'] = rank
-  toJson(data,'modify')
+  toJson(data,'allcourse')
   print(data)
   return result[['title','rank']]
 
--- ofcourse/scripts/tagging.py
+++ ofcourse/scripts/tagging.py
@@ -7,23 +7,24 @@ def toJson(res_dict,save_name):
     with open(f'{save_name}.json', 'w', encoding='utf-8') as file :
         json.dump(res_dict, file, ensure_ascii=False, indent='\t')
 
-# Course data
+# inflearn data
 f = open(f'json/inflearn.json', encoding='UTF-8')
-data = json.loads(f.read())  
+inflearn = json.loads(f.read())  
 # stack data
 f = open(f'json/스택별.json', encoding='UTF-8')
-data2 = json.loads(f.read()) 
+codenary = json.loads(f.read()) 
 # company data
 f = open(f'json/stackDict.json', encoding='UTF-8')
-data3 = json.loads(f.read()) 
+stackDict = json.loads(f.read()) 
+# udemy data
+f = open(f'json/udemy.json', encoding='UTF-8')
+udemy = json.loads(f.read()) 
+# allcourse data
+f = open(f'json/udemy.json', encoding='UTF-8')
+allcourse = json.loads(f.read()) 
 
-# (사전 업데이트 시에만 필요)사전 리스트에 원래 스택 이름 없다면 추가해서 저장
-# for i in data3:
-#   if i not in data3[i]:
-#     data3[i].append(i)
-# toJson(data3,"stackDict")
 
-# 기술 스택 이름 통일
+# 기술 스택 이름 통일(인프런)
 # def run():
 #   for i in data:
 #     if "JavaScript" in data[i]['stacks']:
@@ -35,20 +36,29 @@ data3 = json.loads(f.read())
       #   print(stack)
   # toJson(data,"tmp")
 
+# 강의 데이터 합치기(중복 강의 제거)
+def run():
+  allcourse = {}
+  for i in inflearn:
+    allcourse[i] = inflearn[i]
+  for i in udemy:
+    allcourse[i] = udemy[i]
+
+  toJson(allcourse,"allcourse")
+
+
 
 # 강의 타이틀이나 헤드라인에 기술 사전에 해당하는 기술이 있다면 강의 기술 스택리스트에 추가("R"과 "Go"는 제외)
-def run():
-  # for i in data:
-  #   new_stack = data[i]['stacks']
-  #   for k,v in data3.items():
-  #     if k not in ["Go","R","Java","Spring","Flow","Gin","Realm","Netty","Notion"]:
-  #       for stack in v:
-  #         if stack.lower() in i.lower() or stack.lower() in data[i]['headline'].lower():
-  #           if k not in data[i]['stacks']:
-  #             new_stack.append(k)
-  #             print(i, data[i]['stacks'])
-  #   data[i]['stacks'] = new_stack
-    for i in data3:
-      print(i)
+# def run():
+#   for i in udemy:
+#     new_stack = udemy[i]['stacks']
+#     for k,v in stackDict.items():
+#       if k not in ["Go","R","Java","Spring","Flow","Gin","Realm","Netty","Notion"]:
+#         for stack in v:
+#           if stack.lower() in i.lower() or stack.lower() in udemy[i]['headline'].lower():
+#             if k not in udemy[i]['stacks']:
+#               new_stack.append(k)
+#               print(i, udemy[i]['stacks'])
+#     udemy[i]['stacks'] = new_stack
 
-  # toJson(data,"result")
+#   toJson(udemy,"udemy_update")
