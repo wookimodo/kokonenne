bd0b613ec41a106d062ec7a218aad24fe5684858
jio <jiographie@gmail.com>
2023-01-22 16:46:30 +0900

    front bak

--- ofcourse/scripts/db_save.py
+++ ofcourse/scripts/db_save.py
@@ -31,69 +31,69 @@ def toJson(res_dict,save_name):
 # Course 테이블 저장
 def run():
 
-    # for i in data:
-    #     url = data[i]['url']
-    #     img_url = data[i]['image']
-    #     title = i
-    #     teacher = data[i]['teacher']
-    #     headline = data[i]['headline']
-    #     level = data[i]['level']
-    #     score = data[i]['score']
-    #     courseTime = data[i]['courseTime']
-    #     studentCnt = data[i]['student']
-    #     recommend = data[i]['recommend']
-    #     reviewCnt = data[i]['reviewCnt']
-    #     price = data[i]['price']
-    #     rank = data[i]['rank']
-    #     Course(
-    #         url=url, 
-    #         img_url=img_url, 
-    #         title=title,
-    #         teacher=teacher,
-    #         headline=headline,
-    #         level=level,
-    #         score=score,
-    #         courseTime=courseTime,
-    #         studentCnt=studentCnt,
-    #         recommend=recommend,
-    #         reviewCnt=reviewCnt,
-    #         price=price,
-    #         rank=rank
-    #     ).save()
-    # # Stacks테이블 저장
-    # for i in data2:
-    #     stack = i
-    #     logo = data2[i]['logo']
-    #     assort = data2[i]['assort']
-    #     described = data2[i]['described']
+    for i in data:
+        url = data[i]['url']
+        img_url = data[i]['image']
+        title = i
+        teacher = data[i]['teacher']
+        headline = data[i]['headline']
+        level = data[i]['level']
+        score = data[i]['score']
+        courseTime = data[i]['courseTime']
+        studentCnt = data[i]['student']
+        recommend = data[i]['recommend']
+        reviewCnt = data[i]['reviewCnt']
+        price = data[i]['price']
+        rank = data[i]['rank']
+        Course(
+            url=url, 
+            img_url=img_url, 
+            title=title,
+            teacher=teacher,
+            headline=headline,
+            level=level,
+            score=score,
+            courseTime=courseTime,
+            studentCnt=studentCnt,
+            recommend=recommend,
+            reviewCnt=reviewCnt,
+            price=price,
+            rank=rank
+        ).save()
+    # Stacks테이블 저장
+    for i in data2:
+        stack = i
+        logo = data2[i]['logo']
+        assort = data2[i]['assort']
+        described = data2[i]['described']
         
-    #     Stacks(
-    #         name = stack,
-    #         logo = logo,
-    #         assort = assort,
-    #         described = described            
-    #     ).save()
+        Stacks(
+            name = stack,
+            logo = logo,
+            assort = assort,
+            described = described            
+        ).save()
 
-    # # Course_Stacks 중간테이블
-    # for i in data2:
-    #     for j in data:
-    #         if i in data[j]['stacks']:
-    #             Course_Stacks(course_id=Course.objects.get(title=j).pk,stacks_id=Stacks.objects.get(name=i).pk).save()
+    # Course_Stacks 중간테이블
+    for i in data2:
+        for j in data:
+            if i in data[j]['stacks']:
+                Course_Stacks(course_id=Course.objects.get(title=j).pk,stacks_id=Stacks.objects.get(name=i).pk).save()
 
-    # # Company 테이블
-    # for i in data3:
-    #     name = i
-    #     logo = data3[i]['logo']
-    #     stack_info = data3[i]['stack_info']
+    # Company 테이블
+    for i in data3:
+        name = i
+        logo = data3[i]['logo']
+        stack_info = data3[i]['stack_info']
 
-    #     Company(name=name, logo=logo, stack_info=stack_info).save()
+        Company(name=name, logo=logo, stack_info=stack_info).save()
 
-    # # Company_Stacks 테이블
-    # for i in data3:
-    #     for k,v in data3[i]['stacks'].items():
-    #         if v != None:
-    #             for stack in data3[i]['stacks'][k]:
-    #                 Company_Stacks(company_id=Company.objects.get(name=i).pk,stacks_id=Stacks.objects.get(name=stack[0]).pk).save()
+    # Company_Stacks 테이블
+    for i in data3:
+        for k,v in data3[i]['stacks'].items():
+            if v != None:
+                for stack in data3[i]['stacks'][k]:
+                    Company_Stacks(company_id=Company.objects.get(name=i).pk,stacks_id=Stacks.objects.get(name=stack[0]).pk).save()
 
     # related_stack 테이블
     for i in data2:
