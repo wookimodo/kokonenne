a56268c53ae7145c632a88d172064c2f42492d9d
jw <hbt24@naver.com>
2023-01-24 01:11:15 +0900

    feat : 스택 앱 수정중

--- ofcourse/stack/views.py
+++ ofcourse/stack/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from course.models import Stacks, Related_Stacks, Company, Course_Stacks
-from django.shortcuts import get_object_or_404
+from django.db.models import Q
+
 
 # Create your views here.
 class StackList(ListView): 
@@ -15,27 +16,12 @@ class StackDetail(DetailView):
     model = Stacks
     template_name = 'stack/stack_detail.html'
 
-    # def get_context_data(self, **kwargs):
-    #     context = super(StackDetail, self).get_context_data()
-    #     st =Stacks.objects.filter(id=2).prefetch_related('company_set')[0]
-    #     print(st)
-    #     context['companies'] = st.course_set.all()
-    #     print(context['companies'])
-    #     for i in context['companies']:
-    #         print("cours :", i)
+def search(request):
+    stacks = Stacks.objects.all()
+    search = request.GET.get('search','')
+    if search:
+        stack_list = stacks.filter(name__icontains=search).distinct()
+        return render(request, 'stack/stack_search.html', {'stack':stack_list, 'search':search})
+    else:
+        return render(request,  'stack/stack_search.html')
 
-        # context = super(StackDetail, self).get_context_data()
-        # st =Stacks.objects.filter(id=2).prefetch_related('course_set')[0]
-        # print(st)
-        # context['courses'] = st.course_set.all()
-        # print(context['courses'])
-        # for i in context['courses']:
-        #     print("cours :", i)
-        
-        # context['companies'] = Stacks.objects.filter(id=1).prefetch_related('company_set')
-        # return context
-
-    # def get_context_data(self, **kwargs):
-    #     context = super(StackDetail, self).get_context_data()
-    #     context['test'] = Company.objects.all()
-    #     return context
