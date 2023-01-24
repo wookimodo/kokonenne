63516f928a98d8b844d9e262465503c0be633def
Yeonguk Kim <rudghk1437@naver.com>
2023-01-24 20:04:52 +0900

    feat : groomData 추가 및 랭킹알고리즘 수정

--- ofcourse/scripts/course_ml.py
+++ ofcourse/scripts/course_ml.py
@@ -11,27 +11,16 @@ def toJson(res_dict,save_name):
     with open(f'{save_name}.json', 'w', encoding='utf-8') as file :
         json.dump(res_dict, file, ensure_ascii=False, indent='\t')
 
-# 전체 데이터
+# 전체 강의 데이터 불러오기
 f = open(f'json/allcourse.json', encoding='UTF-8')
 data = json.loads(f.read())  
 
-# 데이터 불러오기(inflearn)
-df1 = pd.read_json("json/inflearn.json")
-df1 = df1.transpose().reset_index().rename(columns={'index':'title'})
-df1 = df1.drop(['headline','url','image','level','recommend','price','rank','courseTime'],axis=1)
-df1 = df1[['title','teacher','stacks','student','reviewCnt','score']]
-df1['website'] = 'inflearn'
-
-# 데이터 불러오기(udemy)
-df2 = pd.read_json("json/udemy.json")
-df2 = df2.transpose().reset_index().rename(columns={'index':'title'})
-df2 = df2.drop(['headline','url','image','level','courseTime','courseCnt','price','recommend'],axis=1)
-df2 = df2.rename(columns={'rating':'score'})
-df2 = df2[['title','teacher','stacks','student','reviewCnt','score']]
-df2['website'] = 'udemy'
-
-# 데이터 합치기
-df = pd.concat([df1,df2],axis=0)
+# 데이터 불러오
+df = pd.read_json(f"json/allcourse.json")
+df = df.transpose().reset_index().rename(columns={'index':'title'})
+df = df.drop(['headline','url','image','level'],axis=1)
+df = df.rename(columns={'rating':'score'})
+df = df[['title','teacher','stacks','student','reviewCnt','score']]
 
 # 리뷰 수 전처리 
 df['reviewCnt'] = [str(i).replace(',','') for i in df['reviewCnt']]
@@ -45,6 +34,46 @@ df['score'] = [None if i=='None' else i for i in df['score']]
 # 타입변환
 df = df.astype({'student':'float','reviewCnt':'float','score':'float'})
 
+# 구름사이트 감정분석 데이터
+f = open(f'json/goorm_sentiment.json', encoding='UTF-8')
+goorm = json.loads(f.read())  
+
+# 데이터프레임으로 변환
+review_data = []
+for title in goorm:
+  for review in goorm[title]:
+    review_data.append([title,review[0],review[1],review[2]])
+df_goorm = pd.DataFrame(review_data,columns=['title','review','sentiment','score'])
+
+# 인프런 사이트 감정분석 데이터
+f = open(f'json/inflearn_sentiment.json', encoding='UTF-8')
+inflearn = json.loads(f.read())  
+
+# 데이터프레임으로 변환
+review_data = []
+for title in inflearn:
+  for review in inflearn[title]:
+    review_data.append([title,review[0],review[1],review[2]])
+df_inflearn = pd.DataFrame(review_data,columns=['title','review','sentiment','score'])
+
+# 감정분석 데이터 합치기
+df_sentiment = pd.concat([df_goorm,df_inflearn],axis=0)
+
+# 인강별로 긍정,부정 비율 구해서 피쳐 추가
+agg_dict = {
+  'score' : [
+        ("positive_ratio", lambda x : (x>0.98).mean()),
+        ("negative_ratio", lambda x : (x<-0.85).mean()),
+    ]
+}
+
+tmp = df_sentiment.groupby('title').agg(agg_dict)
+tmp.columns = tmp.columns.droplevel()
+tmp = tmp.reset_index()
+
+# 전체 강의 데이터프레임에 merge
+df = df.merge(tmp,how="left",on = "title") 
+
 # stack 없는 행 삭제
 idx = df.loc[df['stacks']==""].index
 df = df.drop(idx)
@@ -55,7 +84,8 @@ for i in df['stacks']:
   cat1.append(i[0])
 df['cat1'] = cat1
 # 컬럼 순서 재정의
-df = df[['title','teacher','website','cat1','student','reviewCnt','score']]
+df = df[['title','teacher','cat1','student','reviewCnt','score','positive_ratio','negative_ratio']]
+
 
 # 대분류별로 평균을 구해서 컬럼 추가
 agg_dict = {
@@ -68,7 +98,6 @@ agg_dict = {
           'student' : [
         ("avg_student", 'mean'),
     ],
-
 }
 
 tmp = df.groupby('cat1').agg(agg_dict)
@@ -77,6 +106,7 @@ tmp = tmp.reset_index()
 
 # df에 merge
 df = df.merge(tmp,how="left",on = "cat1") 
+
 # 평점에 대한 결측치는 평균으로 채움
 df["score"] = df["score"].fillna(df["score"].mean())
 df["avg_score"] = df["avg_score"].fillna(df["avg_score"].mean())
@@ -85,6 +115,10 @@ df["avg_score"] = df["avg_score"].fillna(df["avg_score"].mean())
 df["student"] = df["student"].fillna(0)
 df["avg_student"] = df["avg_student"].fillna(0)
 
+# 리뷰가 없는 데이터는 평균으로 채움
+df["positive_ratio"] = df["positive_ratio"].fillna(df["positive_ratio"].mean())
+df["negative_ratio"] = df["negative_ratio"].fillna(df["negative_ratio"].mean())
+
 # 강의시간, 수강생수, 리뷰수, 평점이 각 카테고리의 평균와 얼마나 차이나는지
 df['diffstudent'] = [x-y for x,y in zip(df['student'],df['avg_student'])]
 df['diffreviewCnt'] = [x-y for x,y in zip(df['reviewCnt'],df['avg_reviewCnt'])]
@@ -92,6 +126,7 @@ df['diffscore'] = [x-y for x,y in zip(df['score'],df['avg_score'])]
 
 # 랭킹계산에 필요한 컬럼만 추출
 df_ranking = df.iloc[:,5:]
+
 # 민맥스스케일링
 scaler = MinMaxScaler()
 df_ranking = scaler.fit_transform(df_ranking)
@@ -100,12 +135,12 @@ df_ranking = pd.DataFrame(df_ranking)
 df_ranking.columns = df.iloc[:,5:].columns
 
 # 랭킹df에 title, teacher, cat1추가
-df_ranking[['title','teacher','website']] = df[['title','teacher','website']]
+df_ranking[['title','teacher']] = df[['title','teacher']]
 df_ranking['cat1'] = df['cat1']
-df = df_ranking[['title','teacher','website','cat1','diffstudent','diffreviewCnt','diffscore']]
+df = df_ranking[['title','teacher','cat1','diffstudent','diffreviewCnt','diffscore','positive_ratio','negative_ratio']]
 
 # finalscore 계산
-df['finalscore'] = [a+b+c if website == 'inflearn' else 0.95*(a+b+c) for a,b,c,website in zip(df.loc[:,'diffstudent'],df.loc[:,'diffreviewCnt'],df.loc[:,'diffscore'],df.loc[:,'website'])]
+df['finalscore'] = [a+b+c+d-e for a,b,c,d,e in zip(df.loc[:,'diffstudent'],df.loc[:,'diffreviewCnt'],df.loc[:,'diffscore'],df.loc[:,'positive_ratio'],df.loc[:,'negative_ratio'])]
 df['rank'] = df['finalscore'].rank(ascending=False)
 
 # 기술 스택별로 랭킹반환하는 함수
--- ofcourse/scripts/tagging.py
+++ ofcourse/scripts/tagging.py
@@ -1,5 +1,5 @@
 import json
-from course.models import Course, Stacks, Course_Stacks, Company, Related_Stacks,Company_Stacks
+from course.models import Course, Stack, Course_Stack, Company, Related_Stack,Company_Stack
 
 
 # 제이슨 파일로 저장
@@ -22,6 +22,20 @@ udemy = json.loads(f.read())
 # allcourse data
 f = open(f'json/udemy.json', encoding='UTF-8')
 allcourse = json.loads(f.read()) 
+# goorm data
+f = open(f'json/goorm.json', encoding='UTF-8')
+goorm = json.loads(f.read()) 
+# allcourse data
+f = open(f'json/allcourse.json', encoding='UTF-8')
+allcourse = json.loads(f.read()) 
+
+# goorm_sentiment data
+f = open(f'json/goorm_sentiment.json', encoding='UTF-8')
+goorm_sentiment = json.loads(f.read()) 
+
+# goorm_sentiment data
+f = open(f'json/inflearn_sentiment.json', encoding='UTF-8')
+inflearn_sentiment = json.loads(f.read()) 
 
 
 # 기술 스택 이름 통일(인프런)
@@ -38,11 +52,8 @@ allcourse = json.loads(f.read())
 
 # 강의 데이터 합치기(중복 강의 제거)
 def run():
-  allcourse = {}
-  for i in inflearn:
-    allcourse[i] = inflearn[i]
-  for i in udemy:
-    allcourse[i] = udemy[i]
+  for i in allcourse:
+    allcourse[i] = allcourse[i]
 
   toJson(allcourse,"allcourse")
 
@@ -50,15 +61,26 @@ def run():
 
 # 강의 타이틀이나 헤드라인에 기술 사전에 해당하는 기술이 있다면 강의 기술 스택리스트에 추가("R"과 "Go"는 제외)
 # def run():
-#   for i in udemy:
-#     new_stack = udemy[i]['stacks']
-#     for k,v in stackDict.items():
-#       if k not in ["Go","R","Java","Spring","Flow","Gin","Realm","Netty","Notion"]:
-#         for stack in v:
-#           if stack.lower() in i.lower() or stack.lower() in udemy[i]['headline'].lower():
-#             if k not in udemy[i]['stacks']:
-#               new_stack.append(k)
-#               print(i, udemy[i]['stacks'])
-#     udemy[i]['stacks'] = new_stack
+  # for i in udemy:
+  #   new_stack = udemy[i]['stacks']
+  #   for k,v in stackDict.items():
+  #     if k not in ["Go","R","Java","Spring","Flow","Gin","Realm","Netty","Notion"]:
+  #       for stack in v:
+  #         if stack.lower() in i.lower() or stack.lower() in udemy[i]['headline'].lower():
+  #           if k not in udemy[i]['stacks']:
+  #             new_stack.append(k)
+  #             print(i, udemy[i]['stacks'])
+  #   udemy[i]['stacks'] = new_stack
 
-#   toJson(udemy,"udemy_update")
+  # for i in goorm:
+  #   add_stack = goorm[i]['stacks']
+  #   for k,v in stackDict.items():
+  #     if k not in ["Go","R","Spring","Flow","Gin","Realm","Netty","Notion"]:
+  #       for stack in v:
+  #         if stack.lower() in i.lower() or stack.lower() in goorm[i]['headline'].lower():
+  #           if k not in goorm[i]['stacks']:
+  #             add_stack.append(k)
+  #   goorm[i]['stacks'] = add_stack
+
+
+  # toJson(goorm,"goorm")
